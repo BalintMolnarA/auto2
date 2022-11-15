4292a0da00ea662c6d085c12fd7eea1462e8ce09
Molnár Bálint Attila <molnarbalinta@EOTVOS2.LOCAL>
2022-11-15 15:13:18 +0100

    tablakkesz

--- database/migrations/2022_11_15_140945_create_baleset_idopont.php
+++ database/migrations/2022_11_15_140945_create_baleset_idopont.php
@@ -13,14 +13,10 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
+        Schema::create('baleset_idopont', function (Blueprint $table) {
+            $table->id(b_id);
+            $table->dateTime("Baleset_idopontja");
+            $table->string("Serules_leirasa",1000);
         });
     }
 
@@ -31,6 +27,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('baleset_idopont');
     }
 };
